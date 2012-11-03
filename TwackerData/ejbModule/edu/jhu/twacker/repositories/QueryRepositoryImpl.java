1910b0f2f15cb3d88d28c61d5ce665877af94de4
Disa Mhembere <dmhembe1@jhu.edu>
2012-11-03 14:10:35 -0400

    Undid Dan's unwanted pushes

--- TwackerData/ejbModule/edu/jhu/twacker/repositories/QueryRepositoryImpl.java
+++ TwackerData/ejbModule/edu/jhu/twacker/repositories/QueryRepositoryImpl.java
@@ -7,6 +7,7 @@ package edu.jhu.twacker.repositories;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
